429a77ed23952288720004b050f24b79c3115093
Sameer Magan <prrism@gmail.com>
2018-01-05 15:32:26 +1300

    added passwordless sudo for wheel group

--- conf/nixos/configuration.nix
+++ conf/nixos/configuration.nix
@@ -79,6 +79,8 @@
 
   programs.zsh.enable = true;
 
+  security.sudo.wheelNeedsPassword = false;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.sameer = {
     isNormalUser = true;
