0bf12d7176bd47357304614c09d5938c346f203e
Sameer Magan <sameer.magan@tpicap.com>
2023-12-18 18:47:22 +0000

    Updated conf

--- conf/.config/nvim-take2/after/plugin/lsp.lua
+++ conf/.config/nvim-take2/after/plugin/lsp.lua
@@ -68,3 +68,20 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
 })
+
+--  For deno lsp
+vim.g.markdown_fenced_languages = {
+  "ts=typescript"
+}
+
+local nvim_lsp = require('lspconfig')
+nvim_lsp.denols.setup {
+  -- on_attach = on_attach,
+  root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+}
+
+nvim_lsp.tsserver.setup {
+  -- on_attach = on_attach,
+  root_dir = nvim_lsp.util.root_pattern("package.json"),
+  single_file_support = false
+}
