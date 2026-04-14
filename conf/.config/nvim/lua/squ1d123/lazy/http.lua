3bd57b666fd7a10cd25fdee2cac122d57fdc182b
Sameer Magan <sm.sameermagan@gmail.com>
2026-04-14 15:49:18 +0100

    feat: update lazy packages

--- conf/.config/nvim/lua/squ1d123/lazy/http.lua
+++ conf/.config/nvim/lua/squ1d123/lazy/http.lua
@@ -1,5 +1,6 @@
 return {
   "rest-nvim/rest.nvim",
+  enabled = false,
 
   opts = {
     rocks = {
--- conf/.config/nvim/lua/squ1d123/lazy/treesitter.lua
+++ conf/.config/nvim/lua/squ1d123/lazy/treesitter.lua
@@ -2,12 +2,8 @@ return {
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    -- dependencies = {
-    --   'nvim-treesitter/nvim-treesitter-textobjects',
-    -- },
     lazy = false,
     build = ':TSUpdate',
-    brach = 'master',
     config = function()
       require('nvim-treesitter').install({
         "vimdoc", "javascript", "typescript", "lua", "rust", "bash", "python", "tsx", "go", "hcl", "terraform", "yaml",
