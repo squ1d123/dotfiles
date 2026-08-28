cd699f27e5f2fd34524c7465ae6a6eadd7271a9b
Sameer Magan <sm.sameermagan@gmail.com>
2026-08-28 15:45:04 +0100

    feat: latest changes

--- conf/.config/nvim/lua/squ1d123/lazy/onedark.lua
+++ conf/.config/nvim/lua/squ1d123/lazy/onedark.lua
@@ -6,7 +6,7 @@ return {
     vim.cmd.colorscheme 'onedark'
     require('onedark').setup {
         style = 'warmer',
-        transparent = false, -- We may not like this, however, lets give it a try
+        transparent = true, -- We may not like this, however, lets give it a try
         -- set background color to a much darker black
         colors = {
             bg0 = "#121315"
